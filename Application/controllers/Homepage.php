01d98e127e89f176f9f24596bae3874eed5c8bdd
Matthew Booysen <MatthewBoo@awards.co.za>
2020-01-07 09:27:01 +0200

    intial project

---
+++ Application/controllers/Homepage.php
@@ -0,0 +1,12 @@
+<?php
+  class Homepage extends Controller {
+      
+      function index(){ 
+        echo 'Homepage is working';
+      }
+
+      function error(){
+          echo 'Error';
+      }
+  }
+?>
