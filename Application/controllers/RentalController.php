07baeacffe64332d8b93583da11b5ac9a30106a0
Matthew Booysen <MatthewBoo@awards.co.za>
2020-01-16 10:46:58 +0200

    create Rental added

--- Application/controllers/RentalController.php
+++ Application/controllers/RentalController.php
@@ -22,6 +22,188 @@ class RentalController extends Controller {
     //         exit;
     //     }
     // }
+    //============================================================
+                    //Create or Update
+    //============================================================
+    function create($f3, $params){
 
+        header('Content-type:application/json');
+
+        try{
+
+            $data = json_decode($f3->get('BODY'), true);
+            $custNumber = $params['custNumber'];
+
+            if(empty($data['custNumber']) || empty($data['vehNumber'])){
+                echo json_encode(array(
+                    'success' => false,
+                    'message' => 'Missing fields'
+                ));
+    
+                return;
+            }
+            $customer = new Customer($this->db);
+            $vehicle = new Vehicle($this->db);
+            $rental = new Rental($this->db);
+
+            if(empty($custNumber)){
+                //create
+                $customerResults = $customer->searchViaCustNumber($data['custNumber']);
+                $vehicleResults = $vehicle->searchByvehNumber($data['vehNumber']);
+                $rentalResults = $rental->searchViaCustNumber($data['custNumber']);
+
+                if(empty($customerResults)){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Customer details does not exists'
+                    ));
+                    return;
+                }
+
+                if(empty($vehicleResults)){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Vehicle details does not exists'
+                    ));
+                    return;
+                }
+
+                if(!empty($rentalResults)){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Customer has already has an OUTSTANDING rental'
+                    ));
+                    return;
+                }
+
+                if($customerResults[0]['canRent'] == 1){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Customer has already has an OUTSTANDING rental'
+                    ));
+                    return;
+                }
+
+                if($vehicleResults[0]['availableForRent'] == 1){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Vehicle has already been rented'
+                    ));
+                    return;
+                }
+
+                $data['dateRental'] = date('Y-m-d H:i:s');
+                $data['pricePerDay'] = $vehicleResults[0]['rentalPrice'];
+                //$data['pricePerDay'] = $vehicleResults['rentalPrice'];
+
+                //update customer canRent 
+                $customerData['custNumber'] = $data['custNumber'];
+                $customerData['canRent'] = 1;
+                $customerData['LastRented'] = date('Y-m-d H:i:s');
+                $customerData['firstName'] = $customerResults[0]['firstName'];
+                $customerData['surName'] = $customerResults[0]['surName'];
+
+                //update vehicle available rent
+                $vehicleData['vehNumber'] = $vehicleResults[0]['vehNumber'];
+                $vehicleData['availableForRent'] = 1;
+
+                $customer->create($customerData, 'update');
+                $rental->create($data);
+                $vehicle->create($vehicleData);
+    
+            echo json_encode(array(
+                'success' => true,
+                'message' => 'Rental successfully created'
+            ));
+
+            return;
+            } else {
+                //update
+                $customerResults = $customer->searchViaCustNumber($custNumber);
+                $vehicleResults = $vehicle->searchByvehNumber($data['vehNumber']);
+                $rentalResults = $rental->searchViaRentalNumber($data['rentalNumber']);
+
+                if(empty($customerResults)){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Customer details does not exists'
+                    ));
+                    return;
+                }
+
+                if(empty($vehicleResults)){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Vehicle details does not exists'
+                    ));
+                    return;
+                }
+
+                if(empty($rentalResults)){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'No Rental for the Customer'
+                    ));
+                    return;
+                }
+
+                if($customerResults[0]['canRent'] == 0){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Customer has No rental'
+                    ));
+                    return;
+                }
+
+                if($vehicleResults[0]['availableForRent'] == 0){
+                    echo json_encode(array(
+                        'success' => false,
+                        'message' => 'Vehicle has not been rented'
+                    ));
+                    return;
+                }
+
+                //calc total rental
+                // $dateRental =DateTime::createFromFormat('m-d-Y', $rentalResults[0]['dateRental'])->format('Y-m-d');
+                // $dateRenturned = date_format($data['dateReturned'],"Y/m/d");
+
+                $data['dateReturned'] = date('Y-m-d H:i:s');
+                $dateDiff = round(($rentalResults[0]['dateRental'] - $data['dateReturned']) / (60 * 60 *24)) + 1;
+
+                $PriceMustPay = $dateDiff * $rentalResults[0]['pricePerDay'];
+
+                
+                $data['totalrental'] = $PriceMustPay;
+                $data['pricePerDay'] = $rentalResults[0]['pricePerDay'];
+                //update customer canRent 
+                $customerData['custNumber'] = $data['custNumber'];
+                $customerData['canRent'] = 0;
+            
+                //update vehicle available rent
+                $vehicleData['vehNumber'] = $vehicleResults[0]['vehNumber'];
+                $vehicleData['availableForRent'] = 0;
+            
+                $customer->create($customerData, 'update');
+                $rental->create($data);
+                $vehicle->create($vehicleData);
+
+                $rental->create($data);
+    
+            echo json_encode(array(
+                'success' => true,
+                'message' => 'Rental successfully Returned',
+                'Price' => $PriceMustPay
+            ));
+
+            return;
+            }
+
+        }catch(Exception $e){
+            echo json_encode(array(
+                'success' => false,
+                'message' => $e->getMessage()
+            ));
+        }
+    }
 }
 ?>
--- Application/models/Rental.php
+++ Application/models/Rental.php
@@ -4,8 +4,57 @@
 
      public function __construct(DB\SQL $db)
      {
-         parent::__construct($db, 'customer');
+         parent::__construct($db, 'rental');
      }
-     
+
+     public function create($data){
+        try{
+            $this->load(array('custNumber = ?', $data['custNumber']));
+
+            $this->copyFrom($data);
+            $this->save();
+        }catch(Exception $e){
+            throw new Exception($e);
+        }
+     }
+
+     public function delete($data){
+        try{
+            $this->load(array('custNumber = ?', $data['custNumber']));
+
+            $this->copyFrom($data);
+            $this->save();
+        }catch(Exception $e){
+            throw new Exception($e);
+        }
+     }
+
+     public function searchViaCustNumber($custNumber){
+        try{
+
+            $query = "SELECT * FROM rental WHERE custNumber = '$custNumber' ORDER BY dateRental DESC";
+
+            $result = $this->db->exec($query);
+
+            return $result;
+
+         }catch(Exception $e){
+               throw new Exception($e);
+         }
+     }
+
+     public function searchViaRentalNumber($rentalNumber){
+        try{
+
+            $query = "SELECT * FROM rental WHERE rentalNumber = '$rentalNumber' ORDER BY dateRental DESC";
+
+            $result = $this->db->exec($query);
+
+            return $result;
+
+         }catch(Exception $e){
+               throw new Exception($e);
+         }
+     }
     }
 ?>
